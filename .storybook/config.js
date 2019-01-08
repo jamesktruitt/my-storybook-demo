7bdacdfe695c35e633556e69d19471a28c3119ca
James Truitt <jamesktruitt@gmail.com>
2019-01-08 15:33:06 -0700

    setup storybook integration for create react app

---
+++ .storybook/config.js
@@ -0,0 +1,12 @@
+import { configure } from '@storybook/react';
+import requireContext from 'require-context.macro';
+
+import '../src/index.css';
+
+const req = require.context('../src', true, /.stories.js$/);
+
+function loadStories() {
+  req.keys().forEach(filename => req(filename));
+}
+
+configure(loadStories, module);
